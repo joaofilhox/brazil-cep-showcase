126aaca7d6f78e296c3ef92a5ef8776905a7adb0
João Filho <joaofilholimati@gmail.com>
2025-07-09 14:31:48 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from './components/Header';
+import './styles/reset.scss';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>Olá Mundo!!!!</h1>
+    </>
+  )
+}
+
+export default App
